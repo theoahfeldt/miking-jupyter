4abe4ed05f0941578a91256a0c774f1f8b357e30
Anders Thuné <athune@kth.se>
2020-06-30 12:48:09 +0200

    Add simple MCore Jupyter kernel

---
+++ mcore_kernel/kernel.py
@@ -0,0 +1,39 @@
+from ipykernel.kernelbase import Kernel
+from pexpect import replwrap
+
+
+class McoreKernel(Kernel):
+    implementation = 'IMCore'
+    implementation_version = '0.1'
+    language = 'MCore'
+    language_version = '0.1'
+    language_info = {
+        'name': 'MCore',
+        'mimetype': 'text/plain',
+        'file_extension': '.mc',
+    }
+    banner = """The core language of Miking - a meta language system
+for creating embedded domain-specific and general-purpose languages"""
+
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self.wrapper = replwrap.REPLWrapper("miking repl", ">> ", None, continuation_prompt=" | ")
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        output = self.wrapper.run_command(code + ";;")
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': output}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                # The base class increments the execution count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {}
+                }
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=McoreKernel)
